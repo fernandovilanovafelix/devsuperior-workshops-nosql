0b6e078fb382bf067aa75b7bf71335012f78e9e9
Fernando Felix <fernandovnfelix@gmail.com>
2023-07-01 20:59:28 -0300

    Project workshop-mongodb created

---
+++ WorkshopMongoDB/src/test/java/br/com/devsuperior/WorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
