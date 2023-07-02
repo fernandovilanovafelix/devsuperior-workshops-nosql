ee73ae5151994554687eca3f1c52590032ea6c82
Fernando Felix <fernandovnfelix@gmail.com>
2023-07-01 22:13:20 -0300

    Author, Comment, Post

--- WorkshopMongoDB/src/main/java/br/com/devsuperior/mongodb/config/TestConfig.java
+++ WorkshopMongoDB/src/main/java/br/com/devsuperior/mongodb/config/TestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.devsuperior.mongodb.entities.User;
+import br.com.devsuperior.mongodb.repositories.PostRepository;
 import br.com.devsuperior.mongodb.repositories.UserRepository;
 import jakarta.annotation.PostConstruct;
 
@@ -17,10 +18,14 @@ public class TestConfig {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@PostConstruct
 	public void init() {
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
---
+++ WorkshopMongoDB/src/main/java/br/com/devsuperior/mongodb/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package br.com.devsuperior.mongodb.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.devsuperior.mongodb.entities.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
