a49c93d8ef4931ee3671239b3d0b1a2dee8d6119
Fernando Felix <fernandovnfelix@gmail.com>
2023-07-01 21:12:34 -0300

    Inserting user

---
+++ WorkshopMongoDB/src/main/java/br/com/devsuperior/mongodb/config/TestConfig.java
@@ -0,0 +1,31 @@
+package br.com.devsuperior.mongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.devsuperior.mongodb.entities.User;
+import br.com.devsuperior.mongodb.repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostConstruct
+	public void init() {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
